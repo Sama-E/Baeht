d94f2e99f577586692136513b3d5c01f63300599
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-10-27 18:39:12 -0400

    update

--- client/src/components/Home/CategoryCarousel.jsx
+++ client/src/components/Home/CategoryCarousel.jsx
@@ -10,14 +10,21 @@ const CategoryCarousel = () => {
   return (
     <>
     <Box sx={{ display: { xs: "none", sm:"none", md: "block" }}}>
-      <Slider slidesToShow={3} arrowsScroll={4} centerPadding={40}>
+      <Slider slidesToShow={3} arrowsScroll={4} centerMode={false} centerPadding={80}>
         {catcards.map(catcard => (
           <CategoryCards item={catcard} key={catcard.id} />
         ))}
       </Slider>
     </Box>
-    <Box sx={{ display: { xs: "block", sm: "block", md: "none" }}}>
-      <Slider slidesToShow={1} centerMode={true} centerPadding={60}>
+    <Box sx={{ display: { xs: "none", sm: "block", md: "none" }}}>
+      <Slider slidesToShow={1} arrowsScroll={2} centerMode={true} centerPadding={60}>
+        {catcards.map(catcard => (
+          <CategoryCards item={catcard} key={catcard.id} />
+        ))}
+      </Slider>
+    </Box>
+    <Box sx={{ display: { xs: "block", sm: "none", md: "none" }}}>
+      <Slider slidesToShow={1} arrowsScroll={1} centerMode={true} centerPadding={20}>
         {catcards.map(catcard => (
           <CategoryCards item={catcard} key={catcard.id} />
         ))}

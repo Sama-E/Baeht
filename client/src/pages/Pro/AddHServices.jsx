4887c2fd8b4e1e0f332dac2b6891940df998ac9c
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-12-28 14:14:30 -0500

    Update AddHServices.jsx

--- client/src/pages/Pro/AddHServices.jsx
+++ client/src/pages/Pro/AddHServices.jsx
@@ -4,13 +4,16 @@ import { Box } from '@mui/material'
 const AddHServices = () => {
   return (
     <div>
-    <Box sx={{
-      display: 'flex', 
-      flexDirection: 'row', 
-      justifyContent: 'space-between' 
-    }}>
-      <h1>Add New Service</h1>
-    </Box>
+      <Box sx={{
+        display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'space-between' 
+      }}>
+        <h1>Add New Service</h1>
+      </Box>
+      <Box>
+        
+      </Box>
     </div>
   )
 }

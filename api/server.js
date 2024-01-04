cd548a0293e1ad38f9441112227d82bd773171b2
Sama-E <112719147+Sama-E@users.noreply.github.com>
2024-01-04 10:21:06 -0500

    update models

--- api/server.js
+++ api/server.js
@@ -1,3 +1,41 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-console.log("hi")
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+// MONGODB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// MIDDLEWARE
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+// ROUTES
+
+
+// ERRORS
+// app.use((err, req, res, next) => {
+//   const errorStatus = err.status || 500;
+//   const errorMessage = err.message || "Something went wrong!";
+
+//   return res.status(errorStatus).send(errorMessage);
+// });
+
+// API
+app.listen(8800, () => {
+  connect();
+  console.log("Backend server is running!");
+});
